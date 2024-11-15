1a7c94f0a9b8c9821143fd9a5eb9cdea36cb00eb
ekaba007 <121984038+ekaba007@users.noreply.github.com>
2024-11-15 17:48:57 +0100

    filter durations shorter than 2
    Makes for better normality assumption for modeeling. Also added RMD files in gitignore

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -133,7 +133,7 @@ def run_post_processing() -> pd.DataFrame:
     print("\tfiltering valid data...")
     # Filter out participants with only "valid demographics"
     result = df.loc[(df['age'] >= 5) & (df['age'] <= 59) & (df['DoB'] != 2000) & (
-        df['Gender'] != "OTHER") & (df['duration'] > 0.06), :].copy()
+        df['Gender'] != "OTHER") & (df['duration'] > 0.06) & (df['duration'] < 2), :].copy()
     
     print("\trunning age binning clustering...")
     # Utilize K-Means Clustering to bin ages
